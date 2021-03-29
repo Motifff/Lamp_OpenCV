ba7584e539ff2ce41439ad1fcca9417d6e9f1851
Administrator <Administrator@DESKTOP-GM079TC>
2021-03-29 23:53:05 +0800

    merge functions

--- test/test.cpp
+++ test/test.cpp
@@ -1,62 +1,38 @@
-﻿#include <opencv2/opencv.hpp>
-#include <Windows.h>
-#include <opencv2/highgui.hpp>
-#include <imgproc.hpp>
+﻿// The "Square Detector" program.
+// It loads several images sequentially and tries to find squares in
+// each image
+#include "opencv2/core/core.hpp"
+#include <opencv.hpp>
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/highgui/highgui.hpp"
+
 #include <iostream>
+#include <math.h>
 #include <string.h>
 #include <Leap.h>
-#include <math.h>
+#include <imgproc.hpp>
 
-using namespace std;
 using namespace cv;
+using namespace std;
 using namespace Leap;
 
-int thresh = 50, N = 5;
+Mat leftP, rightP;
+vector<vector<Point> > po;
+
+static void help()
+{
+    cout <<
+        "\nA program using pyramid scaling, Canny, contours, contour simpification and\n"
+        "memory storage to find squares in a list of images\n"
+        "Returns sequence of squares detected on the image.\n"
+        "the sequence is stored in the specified memory storage\n"
+        "Call:\n"
+        "./squares\n"
+        "Using OpenCV version %s\n" << CV_VERSION << "\n" << endl;
+}
+
+int thresh = 40, N = 5;
 const char* wndname = "Square Detection Demo";
-
-void leapMotionTest() {
-
-}
-
-class sampleListener : public Listener{
-public:
-    virtual void onInit(const Controller&);
-    virtual void onConnect(const Controller&);
-    virtual void onDisconnect(const Controller&);
-    virtual void onExit(const Controller&);
-    virtual void onFrame(const Controller&);
-};
-void sampleListener::onInit(const Controller& controller)
-{
-    cout << "Initialized" << endl;
-}
-void sampleListener::onConnect(const Controller& controller)
-{
-    cout << "Connected" << endl;
-}
-void sampleListener::onDisconnect(const Controller& controller)
-{
-    cout << "Disconnected" << endl;
-}
-void sampleListener::onExit(const Controller& controller)
-{
-    cout << "Exited" << endl;
-}
-void sampleListener::onFrame(const Controller& controller)
-{
-    const Frame frame = controller.frame();
-    ImageList images = frame.images();
-    Mat leftMat;
-    Mat rightMat;
-    if (images.count() == 2)
-    {
-        leftMat = Mat(images[0].height(), images[0].width(), CV_8UC1, (void*)images[0].data());
-        rightMat = Mat(images[1].height(), images[1].width(), CV_8UC1, (void*)images[1].data());
-        imshow("leftMat", leftMat);
-        imshow("rightMat", rightMat);
-        waitKey(1);
-    }
-}
 
 // helper function:
 // finds a cosine of angle between vectors
@@ -84,9 +60,10 @@ static void findSquares(const Mat& image, vector<vector<Point> >& squares)
 
 
     // blur will enhance edge detection
-    Mat timg(image);
+    Mat timg = image.clone();
+
     medianBlur(image, timg, 9);
-    Mat gray0(timg.size(), CV_8U), gray;
+    Mat gray0(timg.size(), CV_8UC1), gray;
 
     vector<vector<Point> > contours;
 
@@ -94,7 +71,10 @@ static void findSquares(const Mat& image, vector<vector<Point> >& squares)
     for (int c = 0; c < 3; c++)
     {
         int ch[] = { c, 0 };
-        mixChannels(&timg, 1, &gray0, 1, ch, 1);
+        //basicly our data is actually gray
+        //so the mixchanel function is not usable
+        //mixChannels(&timg, 1, &gray0, 1, ch, 1);
+        gray0 = image.clone();
 
         // try several threshold levels
         for (int l = 0; l < N; l++)
@@ -106,6 +86,7 @@ static void findSquares(const Mat& image, vector<vector<Point> >& squares)
                 // apply Canny. Take the upper threshold from slider
                 // and set the lower to 0 (which forces edges merging)
                 Canny(gray0, gray, 5, thresh, 5);
+                //imshow("gray", gray);
                 // dilate canny output to remove potential
                 // holes between edge segments
                 dilate(gray, gray, Mat(), Point(-1, -1));
@@ -113,7 +94,7 @@ static void findSquares(const Mat& image, vector<vector<Point> >& squares)
             else
             {
                 // apply threshold if l!=0:
-                //     tgray(x,y) = gray(x,y) < (l+1)*255/N ? 255 : 0
+                // tgray(x,y) = gray(x,y) < (l+1)*255/N ? 255 : 0
                 gray = gray0 >= (l + 1) * 255 / N;
             }
 
@@ -157,11 +138,11 @@ static void findSquares(const Mat& image, vector<vector<Point> >& squares)
             }
         }
     }
+
 }
 
-
 // the function draws all the squares in the image
-static void drawSquares(Mat& image, const vector<vector<Point> >& squares)
+void drawSquares(Mat& image, const vector<vector<Point> >& squares)
 {
     for (size_t i = 0; i < squares.size(); i++)
     {
@@ -176,19 +157,91 @@ static void drawSquares(Mat& image, const vector<vector<Point> >& squares)
     imshow(wndname, image);
 }
 
-
-int main(int argc, char* argv[])
+class sampleListener : public Listener {
+public:
+    virtual void onInit(const Controller&);
+    virtual void onConnect(const Controller&);
+    virtual void onDisconnect(const Controller&);
+    virtual void onExit(const Controller&);
+    virtual void onFrame(const Controller&);
+};
+void sampleListener::onInit(const Controller& controller)
 {
-    cout << "Loading" << endl;
+    cout << "Initialized" << endl;
+}
+void sampleListener::onConnect(const Controller& controller)
+{
+    cout << "Connected" << endl;
+}
+void sampleListener::onDisconnect(const Controller& controller)
+{
+    cout << "Disconnected" << endl;
+}
+void sampleListener::onExit(const Controller& controller)
+{
+    cout << "Exited" << endl;
+}
+void sampleListener::onFrame(const Controller& controller)
+{
+    const Frame frame = controller.frame();
+    ImageList images = frame.images();
+    Mat leftMat;
+    Mat rightMat;
+    if (images.count() == 2)
+    {
+        leftMat = Mat(images[0].height(), images[0].width(), CV_8UC1, (void*)images[0].data());
+        rightMat = Mat(images[1].height(), images[1].width(), CV_8UC1, (void*)images[1].data());
+        rightP = rightMat.clone();
+        leftP = leftMat.clone();
+        
+        HandList hands = frame.hands();
+        Hand hand1 = hands[0];
+        Hand hand2 = hands[1];
+        
+        if (hand1.isValid() == true || hand2.isValid() == true) {
+            cout << "found hand" << endl;
+        }
+        else {
+            cout << "not yet" << endl;
+        }
+
+        imshow("wen",rightP);
+        findSquares(rightP, po);
+        drawSquares(rightP, po);
+        waitKey(1);
+    }
+}
+
+
+
+int main(int /*argc*/, char** /*argv*/)
+{
     sampleListener listener;
     Controller leap;
-    
+    //static const char* names[] = { "C:\\Users\\Administrator\\Desktop\\test\\test\\1.jpeg",0 };
+    vector<vector<Point> > squares;
     leap.addListener(listener);
     leap.setPolicy(Leap::Controller::POLICY_BACKGROUND_FRAMES);
     leap.setPolicy(Leap::Controller::POLICY_IMAGES);
-
     cin.get();
     leap.removeListener(listener);
+    /*
+    for (int i = 0; names[i] != 0; i++)
+    {
+        Mat image = imread(names[i], 1);
+        if (image.empty())
+        {
+            cout << "Couldn't load " << names[i] << endl;
+            continue;
+        }
 
+        findSquares(image, squares);
+        drawSquares(image, squares);
+        //imwrite( "out", image );
+        int c = waitKey();
+        if ((char)c == 27)
+            break;
+    }
+    */
     return 0;
 }
